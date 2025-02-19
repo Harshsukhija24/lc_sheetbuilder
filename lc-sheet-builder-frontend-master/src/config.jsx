52907403b66e78c85626e921b97cfc250b1da5a9
harsh sukhija <harsh.sukhija.31.1@mountblue.tech>
2025-02-19 14:38:17 +0530

    feat:update config

--- lc-sheet-builder-frontend-master/src/config.jsx
+++ lc-sheet-builder-frontend-master/src/config.jsx
@@ -1,5 +1,7 @@
 const config = {
-  BACKEND_URL: "http://localhost:5000/",
+  // BACKEND_URL: "http://localhost:5000/",
+  BACKEND_URL:
+    "https://lc-sheet-builder-backend-mcgb3bq6l-prabhxs-projects.vercel.app/",
 };
 
 export default config;
